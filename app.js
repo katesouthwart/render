32f8eb8438e7a28dec60ba282f14a7ea49a229e2
Kaitlyn Myer <kate.southwart@gmail.com>
2023-08-18 15:41:30 +0100

    Update Google OAuth callback URL.

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ passport.deserializeUser(async function (id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/timeline/all"
+  callbackURL: "https://www.rendersocial.io/auth/google/timeline/all"
 },
 async function(accessToken, refreshToken, profile, cb) {
 
